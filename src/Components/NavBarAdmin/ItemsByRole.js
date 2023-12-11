fa4f1f68fc5cd539f4aec26afb5f1cc921910871
Multichiki22 <myguelangel12@gmail.com>
2023-12-10 20:00:21 -0500

    new structure for items by role

--- src/Components/NavBarAdmin/ItemsByRole.js
+++ src/Components/NavBarAdmin/ItemsByRole.js
@@ -1,70 +1,65 @@
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+const entradas = (
+  <NavDropdown title="Entradas" id="basic-nav-dropdown">
+    <NavDropdown.Item href="/nuevaEntrada">Nueva entrada</NavDropdown.Item>
+    <NavDropdown.Item href="/nuevoProducto">Nuevo Producto</NavDropdown.Item>
+  </NavDropdown>
+);
+
+const categorias = (
+  <NavDropdown title="Categorias" id="basic-nav-dropdown">
+    <NavDropdown.Item href="/nuevaCategoria">Nueva categoria</NavDropdown.Item>
+    <NavDropdown.Item href="/categorias">Categorias</NavDropdown.Item>
+  </NavDropdown>
+);
+
+const salidasAdmin = (
+  <NavDropdown title="Salidas" id="basic-nav-dropdown">
+    <NavDropdown.Item href="/vender">Nueva Salida</NavDropdown.Item>
+    <NavDropdown.Divider />
+    <NavDropdown.Item href="/misSalidas">Todas mis salidas</NavDropdown.Item>
+    <NavDropdown.Item href="/salidas">Todas salidas hoy</NavDropdown.Item>
+    <NavDropdown.Divider />
+    <NavDropdown.Item href="/miResumen">Mi resumen Hoy</NavDropdown.Item>
+    <NavDropdown.Item href="/resumen">Resumen Hoy</NavDropdown.Item>
+    <NavDropdown.Divider />
+    <NavDropdown.Item href="/historico">Historico</NavDropdown.Item>
+  </NavDropdown>
+);
+
+const usuarios = (
+  <NavDropdown title="Usuarios" id="basic-nav-dropdown">
+    <NavDropdown.Item href="/usuarios">Usuarios</NavDropdown.Item>
+    <NavDropdown.Item href="/nuevoUsuario">Nuevo Usuarios</NavDropdown.Item>
+  </NavDropdown>
+);
+
+const salidasVendedor = (
+  <NavDropdown title="Salidas" id="basic-nav-dropdown">
+    <NavDropdown.Item href="/vender">Nueva Salida</NavDropdown.Item>
+    <NavDropdown.Item href="/misSalidas">Todas mis salidas</NavDropdown.Item>
+    <NavDropdown.Divider />
+    <NavDropdown.Item href="/miResumen">Mi resumen Hoy</NavDropdown.Item>
+  </NavDropdown>
+);
+
 const navItemsByRole = {
   Admin: [
     <>
-      <NavDropdown title="Entradas" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/nuevaEntrada">Nueva entrada</NavDropdown.Item>
-        <NavDropdown.Item href="/nuevoProducto">
-          Nuevo Producto
-        </NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Categorias" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/nuevaCategoria">Nueva categoria</NavDropdown.Item>
-        <NavDropdown.Item href="/categorias">
-          Categorias
-        </NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Salidas" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/vender">Nueva Salida</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/misSalidas">
-          Todas mis salidas
-        </NavDropdown.Item>
-        <NavDropdown.Item href="/salidas">Todas salidas hoy</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/miResumen">Mi resumen Hoy</NavDropdown.Item>
-        <NavDropdown.Item href="/resumen">Resumen Hoy</NavDropdown.Item>
-      </NavDropdown>
+      {entradas}
+      {salidasAdmin}
     </>,
   ],
   Vendedor: [
-    <NavDropdown title="Salidas" id="basic-nav-dropdown">
-      <NavDropdown.Item href="/vender">Nueva Salida</NavDropdown.Item>
-      <NavDropdown.Item href="/misSalidas">Todas mis salidas</NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item href="/miResumen">Mi resumen Hoy</NavDropdown.Item>
-    </NavDropdown>,
+    {salidasVendedor}
   ],
   SuperAdmin: [
     <>
-      <NavDropdown title="Entradas" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/nuevaEntrada">Nueva entrada</NavDropdown.Item>
-        <NavDropdown.Item href="/nuevoProducto">
-          Nuevo Producto
-        </NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Categorias" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/nuevaCategoria">Nueva categoria</NavDropdown.Item>
-        <NavDropdown.Item href="/categorias">
-          Categorias
-        </NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Salidas" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/vender">Nueva Salida</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/misSalidas">
-          Todas mis salidas
-        </NavDropdown.Item>
-        <NavDropdown.Item href="/salidas">Todas salidas hoy</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/miResumen">Mi resumen Hoy</NavDropdown.Item>
-        <NavDropdown.Item href="/resumen">Resumen Hoy</NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Usuarios" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/usuarios">Usuarios</NavDropdown.Item>
-        <NavDropdown.Item href="/nuevoUsuario">Nuevo Usuarios</NavDropdown.Item>
-      </NavDropdown>
+      {entradas}
+      {salidasAdmin}
+      {categorias}
+      {usuarios}
     </>,
   ],
 };
