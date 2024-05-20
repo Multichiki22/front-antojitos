e270a7ab8aa03af170ff17cbdc6d293c562e3293
Multichiki22 <myguelangel12@gmail.com>
2024-05-19 21:33:39 -0500

    Formatter

--- src/Components/ButtonSelection/ButtonSelection.jsx
+++ src/Components/ButtonSelection/ButtonSelection.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import Button from "react-bootstrap/esm/Button";
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/esm/Button';
 
-const aprovedRoles = ["SuperAdmin", "Admin"];
+const aprovedRoles = ['SuperAdmin', 'Admin'];
 
 function ButtonSelection(props) {
-    const {producto }= props
+  const { producto } = props;
   const [multiButton, setMultiButton] = useState(false);
 
   useEffect(() => {
-    const role = localStorage.getItem("role");
+    const role = localStorage.getItem('role');
     if (role) {
       if (aprovedRoles.includes(role)) {
         setMultiButton(true);
@@ -19,31 +19,22 @@ function ButtonSelection(props) {
   return (
     <div>
       {multiButton ? (
-        <div style={{display: "flex", gap: "1%"}}>
-          <Button
-            style={{ backgroundColor: "#00325b",border:"none" }}
-            href={`/vender?nombre=${producto.nombre}`}
-          >
+        <div style={{ display: 'flex', gap: '1%' }}>
+          <Button style={{ backgroundColor: '#00325b', border: 'none' }} href={`/vender?nombre=${producto.nombre}`}>
             Vender
           </Button>
-{/*           <Button
+          {/*           <Button
             style={{ backgroundColor: "#00325b" }}
             href={`/vender?nombre=${producto.nombre}`}
           >
             Detalles
           </Button> */}
-          <Button
-            style={{ backgroundColor: "#00325b", border:"none" }}
-            href={`/producto/editar/${producto.id}`}
-          >
+          <Button style={{ backgroundColor: '#00325b', border: 'none' }} href={`/producto/editar/${producto.id}`}>
             Editar
           </Button>
         </div>
       ) : (
-        <Button
-          style={{ backgroundColor: "#00325b" }}
-          href={`/vender?nombre=${producto.nombre}`}
-        >
+        <Button style={{ backgroundColor: '#00325b' }} href={`/vender?nombre=${producto.nombre}`}>
           Vender
         </Button>
       )}
--- src/Components/Resumen/Resumen.jsx
+++ src/Components/Resumen/Resumen.jsx
@@ -1,31 +1,27 @@
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import ListGroup from "react-bootstrap/ListGroup";
-import Container from "react-bootstrap/esm/Container";
-import { useState, useEffect } from "react";
-import api from "../../axiosConfig/axios";
-import Cargando from "../Cargando/Cargando";
-import ErrorModal from "../ErrorModal/ErrorModal";
+import Container from 'react-bootstrap/esm/Container';
+import { useState, useEffect } from 'react';
+import api from '../../axiosConfig/axios';
+import Cargando from '../Cargando/Cargando';
+import ErrorModal from '../ErrorModal/ErrorModal';
+import ResumenGeneral from '../ResumenGeneral/ResumenGeneral';
 
 function Resumen() {
   const [modal, setModal] = useState(false);
-  const [tituloError, setTituloError] = useState("");
-  const [cuerpoError, setCuerpoError] = useState("");
+  const [tituloError, setTituloError] = useState('');
+  const [cuerpoError, setCuerpoError] = useState('');
   const handleClose = () => setModal(false);
   const [loading, setLoading] = useState(false);
-  const [resumen, setResumen] = useState({cantidadDeProductos: "??", notas: [], valorDelDia: "??"});
+  const [resumen, setResumen] = useState({ motivo : {}, notas: []});
 
   const getResumen = () => {
     api
-      .get("salidas/hoy/resumen")
+      .get('salidas/hoy/resumen')
       .then((response) => {
         setResumen(response.data);
       })
       .catch((error) => {
-        setTituloError("Error con el servidor");
-        setCuerpoError(
-          `Ha ocurrido un error con el servidor intentalo mas tarde: \n Error Name: ${error.message}`
-        );
+        setTituloError('Error con el servidor');
+        setCuerpoError(`Ha ocurrido un error con el servidor intentalo mas tarde: \n Error Name: ${error.message}`);
         setModal(true);
       })
       .finally(() => {
@@ -35,38 +31,12 @@ function Resumen() {
   };
 
   useEffect(() => {
-    getResumen()
+    getResumen();
   }, []);
   return (
     <Container>
-      <ErrorModal
-        show={modal}
-        handleClose={handleClose}
-        title={tituloError}
-        body={cuerpoError}
-      />
-      {loading ? (
-        <Cargando />
-      ) : (
-        <Row className="justify-content-center">
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>Resumen hoy: </Card.Title>
-              <Card.Text>{`Cantidad de productos: ${resumen.cantidadDeProductos}`}</Card.Text>
-              <Card.Text>{`Producido del dia: ${resumen.valorDelDia}`}</Card.Text>
-              <Card.Subtitle>Notas:</Card.Subtitle>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              {resumen.notas.map((nota)=>{
-                if (nota !== "") {
-                  return <ListGroup.Item>{nota}</ListGroup.Item>
-                }
-                return null
-              })}
-            </ListGroup>
-          </Card>
-        </Row>
-      )}
+      <ErrorModal show={modal} handleClose={handleClose} title={tituloError} body={cuerpoError} />
+      {loading ? <Cargando /> : <ResumenGeneral resumen={resumen}></ResumenGeneral>}
     </Container>
   );
 }
